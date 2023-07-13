fdf8b4322a643c465bed34b5f2a0d6ed73a1ab64
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2023-07-13 22:28:04 +1000

    main

---
+++ mt first program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            Console.ReadLine();
+        }
+    }
+}
